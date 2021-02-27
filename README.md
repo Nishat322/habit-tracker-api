20ec3d9a6434ed5fb10dd13dcd227782dd841a8c
Nishat Hoque <hoquen0397@gmail.com>
2021-02-27 15:40:57 -0500

    migrations and seeding the table;

---
+++ README.md
@@ -0,0 +1,17 @@
+# Habit Tracker
+
+Capstone Project
+
+## Scripts
+
+Start the application `npm start`
+
+Start nodemon for the application `npm run dev`
+
+Run the tests `npm test`
+
+Seed the Database `psql -U HabitUser -d habit_tracker -f ./seeds/seed.habits.sql`
+
+## Deploying
+
+When your new project is ready for deployment, add a new Heroku application with `heroku create`. This will make a new git remote called "heroku" and you can then `npm run deploy` which will push to this remote's main branch.
---
+++ seeds/seed.habits.sql
@@ -0,0 +1,7 @@
+INSERT INTO habits (title, description, motivation, goal, daysCompleted)
+VALUES 
+    ('Drink Water',             'Drink 4 glasses of water every day',   'Thirst Quenched!',     28,     3),
+    ('Decrease Screen Time',    'Use screens for less than 5 hours',    'Yay!!',                60,     0),
+    ('Meditate',                'Meditate for 2 hours a day',           'So Peaceful',          28,     4),
+    ('Study',                   'Study 5 hours a day',                  'Learned so much!!',    12,     0),
+    ('Draw',                    'Draw something every day',             NUll,                   30,     2);
